aaa6259b4b23797de7b48bc81360b94f2d5945d2
Michael18811380328 <1822852997@qq.com>
2018-08-24 20:50:05 +0800

    combine commits test1 test2 test3 test4 merge-test1 and merge-2

--- js/sleep.js
+++ js/sleep.js
@@ -1,10 +1,25 @@
   // for loading page testing
-  sleep = (numberMillis) => { 
-    var now = new Date(); 
-    var exitTime = now.getTime() + numberMillis; 
-    while (this.state.loading == true) { 
-      now = new Date(); 
-      if (now.getTime() > exitTime) 
-      return; 
-    } 
+  sleep = (numberMillis) => {
+    var now = new Date();
+    var exitTime = now.getTime() + numberMillis;
+    while (this.state.loading == true) {
+      now = new Date();
+      if (now.getTime() > exitTime)
+      return;
+    }
   }
+
+
+
+  e = document.currentdenfjejejbehcbcdcdll
+      Script ? document.currentScript.src: function() {
+        for (var
+
+e = document.currentdenfjejejbehcbcdcdll
+      Script cument.currentScript.src: function() {
+        for (var
+
+
+        e = document.currentdenfjejejbehcbcdcdll
+      Script ? document.currentScript.src: function() {
+        for (var
