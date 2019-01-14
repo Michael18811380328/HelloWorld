7daa14d286eb69548dd14c16809152156001f634
Michael An <1822852997@qq.com>
2019-01-14 17:31:55 +0800

    a

---
+++ test.js
@@ -0,0 +1,6 @@
+// when your github SSH is not valid
+// 1 check public key and private key (local keys)
+// 2 https://help.github.com/articles/error-permission-denied-publickey/
+// 3 use 'ssh -T Michael18811380328@github.com' to test if you have access to upload files.
+// 4 use 'ssh -T git@github.com' to verify your account. when its response is 'Hi username! You've successfully authenticated...' you can set new name and email then 'git push origin master'
+
