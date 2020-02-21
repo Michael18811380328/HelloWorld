2334a06c0ac8e314c8d0aeb312da50a87ff92462
Michael An <2331806369@qq.com>
2020-02-21 11:04:22 +0800

    remove (#47)

--- 2019-8/time-picker-test/tests/index.js
+++
@@ -1,4 +0,0 @@
-import '../assets/index.less';
-import './TimePicker.spec';
-import './Header.spec';
-import './Select.spec';
--- practice/2019-8/blob.js
+++
@@ -1,9 +0,0 @@
-import blob from '../lib/Blob.js';
-
-let blob1 = new Blob([123]);
-console.log(blob1);
-
-var blob2 = blob1.slice(1);
-console.log(blob2);
-
-//blob
